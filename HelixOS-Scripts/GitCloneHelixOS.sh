4a071a858463d0dc46e9bb59eb0b5fb0602b3f75
Russ Bernhardt <russell.bernhardt@gmail.com>
2017-04-07 10:01:01 -0700

    Just some restructuring and commenting out prebuilts with a note. Need to test this still.

--- HelixOS-Scripts/GitCloneHelixOS.sh
+++ HelixOS-Scripts/GitCloneHelixOS.sh
@@ -1,41 +1,31 @@
-git clone https://github.com/HelixOS/abi.git ./abi/
-git clone https://github.com/HelixOS/art.git ./art/
-git clone https://github.com/HelixOS/bionic.git ./bionic/
-git clone https://github.com/HelixOS/bootable.git ./bootable/
-git clone https://github.com/HelixOS/build.git ./build/
-git clone https://github.com/HelixOS/dalvik.git ./dalvik/
-git clone https://github.com/HelixOS/developers.git ./developers/
-git clone https://github.com/HelixOS/development.git ./development/
-git clone https://github.com/HelixOS/device.git ./device/
-git clone https://github.com/HelixOS/external.git ./external/
-git clone https://github.com/HelixOS/frameworks.git ./frameworks/
-git clone https://github.com/HelixOS/hardware.git ./hardware/
-git clone https://github.com/HelixOS/kernel.git ./kernel/
-git clone https://github.com/HelixOS/libcore.git ./libcore/
-git clone https://github.com/HelixOS/libnativehelper.git ./libnativehelper/
-git clone https://github.com/HelixOS/ndk.git ./ndk/
-git clone https://github.com/HelixOS/packages.git ./packages/
-git clone https://github.com/HelixOS/pdk.git ./pdk/
-git clone https://github.com/HelixOS/sdk.git ./sdk/
-git clone https://github.com/HelixOS/system.git ./system/
-git clone https://github.com/HelixOS/tools.git ./tools/
-git clone https://github.com/HelixOS/vendor.git ./vendor/
-mkdir prebuilts
-git clone https://github.com/HelixOS/prebuilts-android-emulator.git ./prebuilts/android-emulator/
-git clone https://github.com/HelixOS/prebuilts-clang.git ./prebuilts/clang/
-git clone https://github.com/HelixOS/prebuilts-devtools.git ./prebuilts/devtools/
-git clone https://github.com/HelixOS/prebuilts-eclipse.git ./prebuilts/eclipse/
-git clone https://github.com/HelixOS/prebuilts-gcc.git ./prebuilts/gcc/
-git clone https://github.com/HelixOS/prebuilts-gradle-plugin.git ./prebuilts/gradle-plugin/
-git clone https://github.com/HelixOS/prebuilts-libs.git ./prebuilts/libs/
-git clone https://github.com/HelixOS/prebuilts-maven_repo.git ./prebuilts/maven_repo/
-git clone https://github.com/HelixOS/prebuilts-misc.git ./prebuilts/misc/
-git clone https://github.com/HelixOS/prebuilts-ndk.git ./prebuilts/ndk/
-git clone https://github.com/HelixOS/prebuilts-python.git ./prebuilts/python/
-git clone https://github.com/HelixOS/prebuilts-qemu-kernel.git ./prebuilts/qemu-kernel/
-git clone https://github.com/HelixOS/prebuilts-sdk.git ./prebuilts/sdk/
-git clone https://github.com/HelixOS/prebuilts-tools.git ./prebuilts/tools/
-git clone https://github.com/HelixOS/HelixOS.git ./tmp
+#!/bin/bash
+echo "Downloading HelixOS..."
+git clone https://github.com/Codemule-io/HelixOS.git ./tmp/
+
+for dir in abi art bionic bootable build dalvik developers development device external frameworks hardware kernel libcore libnativehelper ndk packages pdk sdk system tools vendor
+do
+	echo "Downloading ${dir}..."
+	git clone https://github.com/Codemule-io/${dir}.git ./${dir}/
+done
+
+# TODO I have the Android 5.1.1 repo sync'd elsewhere with a symlink pointing ./prebuilts to its prebuilts
+#mkdir prebuilts
+#git clone https://github.com/HelixOS/prebuilts-android-emulator.git ./prebuilts/android-emulator/
+#git clone https://github.com/HelixOS/prebuilts-clang.git ./prebuilts/clang/
+#git clone https://github.com/HelixOS/prebuilts-devtools.git ./prebuilts/devtools/
+#git clone https://github.com/HelixOS/prebuilts-eclipse.git ./prebuilts/eclipse/
+#git clone https://github.com/HelixOS/prebuilts-gcc.git ./prebuilts/gcc/
+#git clone https://github.com/HelixOS/prebuilts-gradle-plugin.git ./prebuilts/gradle-plugin/
+#git clone https://github.com/HelixOS/prebuilts-libs.git ./prebuilts/libs/
+#git clone https://github.com/HelixOS/prebuilts-maven_repo.git ./prebuilts/maven_repo/
+#git clone https://github.com/HelixOS/prebuilts-misc.git ./prebuilts/misc/
+#git clone https://github.com/HelixOS/prebuilts-ndk.git ./prebuilts/ndk/
+#git clone https://github.com/HelixOS/prebuilts-python.git ./prebuilts/python/
+#git clone https://github.com/HelixOS/prebuilts-qemu-kernel.git ./prebuilts/qemu-kernel/
+#git clone https://github.com/HelixOS/prebuilts-sdk.git ./prebuilts/sdk/
+#git clone https://github.com/HelixOS/prebuilts-tools.git ./prebuilts/tools/
+#git clone https://github.com/HelixOS/HelixOS.git ./tmp
+
 rsync -av ./tmp/ ./
 rm -rf ./tmp
 mkdir external/chromium_org/third_party/angle/.git
